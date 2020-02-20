56c686a621e5b6821a529b08c9e8c623c42ac794
bozin1990 <a0930832512@gmail.com>
2020-02-20 21:28:03 +0800

    改善新增 刪除功能

--- src/tw/dp103g4/location/InfoLocationDaoMySql.java
+++ src/tw/dp103g4/location/InfoLocationDaoMySql.java
@@ -37,8 +37,8 @@ public class InfoLocationDaoMySql implements InfoLocationDao{
 					Double latitude = rs.getDouble(4);
 					String name = rs.getString(5);
 					String content = rs.getString(6);
-					InfoLocation infoLocation = new InfoLocation(id, partyId, userId, latitude, longitude, name, content);
-					infoLocationList.add(infoLocation);
+					InfoLocation location = new InfoLocation(id, partyId, userId, latitude, longitude, name, content);
+					infoLocationList.add(location);
 				}
 			}
 			return infoLocationList;
@@ -60,22 +60,53 @@ public class InfoLocationDaoMySql implements InfoLocationDao{
 		String sql = "insert into Location (party_id, user_id, longitude, latitude, location_name, location_content)"
 				+ " value (?, ?, ?, ?, ?, ?);";
 		//查詢下⼀個準備產⽣的號碼
-		String idSql = "SELECT AUTO_INCREMENT FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'EcoParty' AND TABLE_NAME = 'Location';";
-		try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-				PreparedStatement psId = connection.prepareStatement(idSql);
-				PreparedStatement ps = connection.prepareStatement(sql);) {
-			ResultSet rs = psId.executeQuery();
-			if (rs.next()) {
-				ps.setInt(1, location.getPartyId());
-				ps.setInt(2, location.getUserId());
-				ps.setDouble(3, location.getLongitude());
-				ps.setDouble(4, location.getLatitude());
-				ps.setString(5, location.getName());
-				ps.setString(6, location.getContent());
-				count = ps.executeUpdate() == 0 ? 0 : rs.getInt(1);
+		String idSql = "SELECT last_insert_id();";
+		Connection connection = null;
+		PreparedStatement psId = null;
+		PreparedStatement ps = null;
+		try {
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			connection.setAutoCommit(false);
+			psId = connection.prepareStatement(idSql);
+			ps = connection.prepareStatement(sql);
+			ps.setInt(1, location.getPartyId());
+			ps.setInt(2, location.getUserId());
+			ps.setDouble(3, location.getLongitude());
+			ps.setDouble(4, location.getLatitude());
+			ps.setString(5, location.getName());
+			ps.setString(6, location.getContent());
+			count = ps.executeUpdate();
+			if (count == 0) {
+				connection.rollback();
+				return count;
+			} else {
+				ResultSet rs = psId.executeQuery();
+				if (rs.next()) {
+					count = rs.getInt(1);
+					connection.commit();
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		} finally {
+			try {
+				if (connection != null) {
+					connection.close();
+				}
+				if (ps != null) {
+					ps.cancel();
+				}
+				if (psId != null) {
+					psId.cancel();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		return count;
 	}
