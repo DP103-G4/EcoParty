ed924aee9da080a8143c9361df3aeefc7618de35
jiantingyu <k12312360@gmail.com>
2020-03-09 19:13:56 +0800

    no message

--- src/tw/dp103g4/party/PartyServlet.java
+++ src/tw/dp103g4/party/PartyServlet.java
@@ -136,22 +136,26 @@ public class PartyServlet extends HttpServlet {
 					coverImg = Base64.getMimeDecoder().decode(coverImgBase64);
 				}
 			}
-			
-			String imgsJson = jsonObject.get("imagesBase64").getAsString();
-			List<String> imagesBase64 = gson.fromJson(imgsJson, new TypeToken<List<String>>() {}.getType());
-
 			int count = 0;
-			count = partyDao.insert(party, coverImg);
+			int ai = 0;
+
+			ai = partyDao.insert(party, coverImg);
 			
-			byte[] image = null;
-			if (jsonObject.get("imagesBase64") != null) {
-				for (String imgBase64: imagesBase64) {
-					if (imgBase64 != null && !imgBase64.isEmpty()) {
-						image = Base64.getMimeDecoder().decode(imgBase64);
-						count = reviewImgDao.insert(party.getId(), image);
+			if (ai != 0) {
+				
+				String imgsJson = jsonObject.get("imagesBase64").getAsString();
+				List<String> imagesBase64 = gson.fromJson(imgsJson, new TypeToken<List<String>>() {}.getType());
+			
+				byte[] image = null;
+				if (jsonObject.get("imagesBase64") != null) {
+					for (String imgBase64: imagesBase64) {
+						if (imgBase64 != null && !imgBase64.isEmpty()) {
+							image = Base64.getMimeDecoder().decode(imgBase64);
+							count = reviewImgDao.insert(ai, image);
+						}
 					}
-				}
-			} 
+				} 
+			}
 			
 			writeText(response, String.valueOf(count));
 		} else if (action.equals("partyUpdate")) {
