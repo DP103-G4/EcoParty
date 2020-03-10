7e2b24364bbcbdde6a3620f59a129dcb4745374e
diehawk951 <diehawk951@gmail.com>
2020-03-10 15:15:55 +0800

    no message

--- src/tw/dp103g4/user/UserDaoImpl.java
+++ src/tw/dp103g4/user/UserDaoImpl.java
@@ -9,8 +9,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.print.attribute.standard.MediaSize.ISO;
-
 public class UserDaoImpl implements UserDao {
 
 	public UserDaoImpl() {
